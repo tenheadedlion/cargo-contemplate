91f21dcbac4c9fd57edfaa48faef06b478b8e2c3
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-09-28 15:36:05 +0800

    make it work

---
+++ README.md
@@ -0,0 +1,26 @@
+# A Cargo Subcommand For Fast Project Setup
+
+## Install
+
+```
+cargo install cargo-contemplate
+```
+
+## Usage
+
+```shell
+cargo-contemplate <class>
+```
+
+Currently supported classes: 
+
+- phat-contract
+
+## Examples
+
+
+```shell
+cargo contemplate phat-contract
+````
+
+The command above will create a directory with the name "phat-contract-start" in the current directory
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,73 @@
 use clap::{Parser, Subcommand};
+use fs_extra::dir::CopyOptions;
 use git2::build::{CheckoutBuilder, RepoBuilder};
 use git2::{FetchOptions, Progress, RemoteCallbacks};
+use phf::phf_map;
+use rand::{distributions::Alphanumeric, Rng};
 use std::cell::RefCell;
+use std::env;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
+use url::{Host, Position, Url};
+
+fn base_name(url: &str) -> Result<String, Error> {
+    let parsed_url = Url::parse(url).map_err(|_| Error::CannotParseUrl)?;
+    let base = Path::new(parsed_url.path())
+        .file_stem()
+        .map(Ok)
+        .unwrap_or_else(|| Err(Error::CannotParseUrl))?;
+    Ok(base.to_str().unwrap().to_string())
+}
+
+fn random_path() -> PathBuf {
+    let rnd_path: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(7)
+        .map(char::from)
+        .collect();
+    Path::join(Path::new("/tmp"), rnd_path)
+}
+
+static URLS: phf::Map<&'static str, &'static str> = phf_map! {
+    "phat-contract" => "https://github.com/tenheadedlion/phat-contract-starter.git",
+};
+
+#[derive(Debug)]
+enum Error {
+    NoSuchClass,
+    CannotParseUrl,
+    FileSystemFault,
+    FileSystemRename,
+    FileSystemRemoveDir,
+    GitFault,
+}
 
 #[derive(Parser)]
 struct Args {
-    #[clap(name = "url")]
-    arg_url: String,
-    #[clap(name = "path")]
-    arg_path: String,
+    #[clap(name = "class")]
+    class: String,
+}
+
+struct Context {
+    url: String,
+    tmp_path: PathBuf,
+    path: String,
+    current_dir: PathBuf,
+}
+
+impl TryFrom<Args> for Context {
+    type Error = Error;
+    fn try_from(args: Args) -> Result<Self, Self::Error> {
+        match URLS.get(&args.class) {
+            Some(url) => Ok(Context {
+                url: url.to_string(),
+                tmp_path: random_path(),
+                path: base_name(url)?,
+                current_dir: env::current_dir().map_err(|_| Error::FileSystemFault)?,
+            }),
+            None => Err(Error::NoSuchClass),
+        }
+    }
 }
 
 struct State {
@@ -65,7 +122,7 @@ fn print(state: &mut State) {
     io::stdout().flush().unwrap();
 }
 
-fn run(args: &Args) -> Result<(), git2::Error> {
+fn run(ctx: &Context) -> Result<(), Error> {
     let state = RefCell::new(State {
         progress: None,
         total: 0,
@@ -92,10 +149,30 @@ fn run(args: &Args) -> Result<(), git2::Error> {
 
     let mut fo = FetchOptions::new();
     fo.remote_callbacks(cb);
+    println!("{} -> {}", &ctx.url, &ctx.tmp_path.display());
     RepoBuilder::new()
         .fetch_options(fo)
         .with_checkout(co)
-        .clone(&args.arg_url, Path::new(&args.arg_path))?;
+        .clone(&ctx.url, &ctx.tmp_path)
+        .map_err(|_| Error::GitFault)?;
+
+    println!("{} ->  {}", &ctx.tmp_path.display(), &ctx.path);
+    let options = CopyOptions::new();
+    fs_extra::dir::copy(&ctx.tmp_path, &ctx.current_dir, &options).map_err(|e| {
+        println!("{}", e);
+        Error::FileSystemFault
+    })?;
+
+    std::fs::rename(Path::new(&ctx.tmp_path).file_name().unwrap(), &ctx.path).map_err(|e| {
+        println!("{}", e);
+        Error::FileSystemRename
+    })?;
+    
+    std::fs::remove_dir_all(Path::join(Path::new(&ctx.path), ".git")).map_err(|e| {
+        println!("{}", e);
+        Error::FileSystemRemoveDir
+    })?;
+
     println!();
 
     Ok(())
@@ -103,8 +180,6 @@ fn run(args: &Args) -> Result<(), git2::Error> {
 
 fn main() {
     let args = Args::parse();
-    match run(&args) {
-        Ok(()) => {}
-        Err(e) => println!("error: {}", e),
-    }
+    let context = Context::try_from(args).unwrap();
+    run(&context).unwrap();
 }
